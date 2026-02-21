9889ae0c9e7b3bbf6b48363d8201530ebb4e6633
karthiga <karthigamanikandan3011@gmail.com>
2026-02-21 22:22:57 +0530

    Add Stage 1 calculator program

---
+++ Karthiga_stage1.py
@@ -0,0 +1,20 @@
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+operator=input("enter operator (add,sub,mul,div): ")
+if operator=="add":
+    result=num1+num2
+    print("Result: ",result)
+elif operator == "sub":
+    result=num1-num2
+    print("Result: ",result)
+elif operator== "mul":
+    result=num1*num2
+    print("Result: ",result)
+elif operator=="div":
+    if num2==0:
+        print("error: division by 0 is not allowed")
+    else: 
+        result =num1/num2
+        print("Result: ",result)
+else:
+    print("Invalid operator")
